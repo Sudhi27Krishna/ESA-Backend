3533ccde0feac5a526eb4ec6a92f86453dacc946
Sudhi Krishna N A <94775649+Sudhi27Krishna@users.noreply.github.com>
2023-05-07 20:32:14 +0530

    Update Schedule.js

--- models/Schedule.js
+++ models/Schedule.js
@@ -2,40 +2,40 @@ const mongoose = require('mongoose');
 
 const ScheduleSchema = new mongoose.Schema({
 
-    user:{
-        type:String,
-        required:true,
-        ref:'User'
+    user: {
+        type: String,
+        required: true,
+        ref: 'User'
     },
     sem: {
         type: Number,
-        required:[true,'Provide semester number'],
+        required: [true, 'Provide semester number'],
     },
-    date:{
-      type:Date,
-      required:[true,'Provide Date'],
-      index: { expires: '10d' },
-
+    date: {
+        type: Date,
+        required: [true, 'Provide Date'],
+        index: { expires: '10d' },
+    },
+    time: {
+        type: String,
+        required: [true, 'Provide Time'],
+        index: { expires: '10d' },
     },
     branch: {
         type: String,
-        required:[true,'Provide Branch'],
-
+        required: [true, 'Provide Branch'],
     },
     slot: {
         type: String,
-        required:[true,'Provide slot info'],
-
+        required: [true, 'Provide slot info'],
     },
-   
+
     subcode: {
         type: String,
-        required:[true,'Provide subcode info'],
-
-        
+        required: [true, 'Provide subcode info'],
     }
 },
-{ timestamps: true }
+    { timestamps: true }
 );
 
 module.exports = new mongoose.model('Schedule', ScheduleSchema);
