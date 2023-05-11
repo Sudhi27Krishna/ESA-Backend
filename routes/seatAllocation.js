61c034a4a3adb5604165cc91b4036997b347ff1b
Arjun <parjununni@gmail.com>
2023-05-11 16:47:54 +0530

    date drop

--- routes/seatAllocation.js
+++ routes/seatAllocation.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const seatAllocationController = require('../controllers/seatAllocationController');
 
-router.get('/get-exams',seatAllocationController.getExams) //get all exams on specific date and time
+router.get('/dates',seatAllocationController.dates)
+      .get('/get-exams',seatAllocationController.getExams) //get all exams on specific date and time
       .get('/get-rooms',seatAllocationController.getRooms); //get all rooms allocated for exams
 
 module.exports = router;
