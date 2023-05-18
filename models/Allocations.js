1ea0f196fca6271456146ffab7af40af4b7546d7
Sudhi Krishna N A <sudhi27krishna@gmail.com>
2023-05-18 14:26:58 +0530

    Modified rooms field with {room_no, capacity} object array

--- models/Allocations.js
+++ models/Allocations.js
@@ -13,9 +13,9 @@ const AllocationSchema = new mongoose.Schema({
 
     },
     rooms: {
-        type: [String],
+        type: [{ room_no: String, capacity: Number }],
         required: [true, 'Provide room array'],
-    },   
+    },
 },
     { timestamps: true }
 );
